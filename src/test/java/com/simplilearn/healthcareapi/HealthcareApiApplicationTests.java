1733a0c8c1d53a701925a7ff462b369a874037c7
Solomon, David <david.solomon@dell.com>
2022-06-08 10:26:40 -0400

    initial

---
+++ src/test/java/com/simplilearn/healthcareapi/HealthcareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.healthcareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
